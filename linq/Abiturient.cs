136ceea6db97a359b28b28c036a0e28466db0c4f
rusya <dnepr.ukrainec@gmail.com>
2019-10-29 15:30:07 +0200

    Добавьте файлы проекта.

---
+++ linq/Abiturient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq
+{
+    class Abiturient
+    {
+        public string Surname { get; set; }
+        public int School { get; set; }
+        public int Year { get; set; }
+
+        public Abiturient(string surname,int school,int year)
+        {
+            Surname = surname;
+            School = school;
+            Year = year;
+        }
+
+    }
+}
